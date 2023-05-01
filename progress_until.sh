093acb2dc00144dfc138467a9598d149d7525643
stas-kuznetsov <89393832+stas-kuznetsov@users.noreply.github.com>
2023-05-01 16:34:52 +0300

    Update progress_until.sh

--- progress_until.sh
+++ progress_until.sh
@@ -8,10 +8,12 @@ _w_progress_until()
 {
 
   local -
-  set -o errexit     # exit immediately if command has non-zero status
-  set -o nounset     # if varuable not set - this is an error
-  set -o pipefail    #
+  set -o errexit
+  set -o nounset
+  set -o pipefail
 
+  ## Draw progress bar executing code passed in arguments while it returns false.
+  #
   ## Draw progress bar executing code passed in arguments while it returns false.
   #
   ## Arguments
@@ -21,6 +23,7 @@ _w_progress_until()
   #
   ## Sample
   # _w_progress_until 30 0.25 ssh server1   - tries to connect to server1 with SSH every 0.25 seconds with timeout 30 seconds. Drows progress bar in persant command SSH has non-zero status
+
 
   local timeout=$1
   local period=$2
@@ -33,40 +36,39 @@ _w_progress_until()
   set +o nounset
   set +o pipefail
 
-   _w_progress 0
+  _w_progress 0
   # eval "$@"
-  output=$( eval "$@" 2>/dev/null )    # read passed parameters of function _w_progress and nothing to output
+  output=$( eval "$@" 2>/dev/null )
   err=$?
-  if [[ $err -eq 0 ]]     # if no errors
+  if [[ $err -eq 0 ]]
   then
-  _w_progress 100         # run function _w_progress with his argument 100
-  printf "%s" "$output"   # print argumant _w_progress
+  _w_progress 100
+  printf "%s" "$output"
   return
   fi
 
   now_time=$( date +%s )
   elapsed_time=$(( $now_time - $start_time ))
 
-  while (( $err != 0 && $elapsed_time < $timeout ))     # while error and elapsed < timeout
+  while (( $err != 0 && $elapsed_time < $timeout ))
   do
 
-  _w_sleep $period       # run function _w_sleep with $2 argument
+  _w_sleep $period
   # qqq2 : make argument $period proper
   # qqq2 : should return nonzero code if fail as well as print stdderr
 
   now_time=$( date +%s )
   elapsed_time=$(( $now_time - $start_time ))
-  _w_progress $(( 100 * $elapsed_time / $timeout ))   # calculate persent of elapsed time, run function _w_progress
+  _w_progress $(( 100 * $elapsed_time / $timeout ))
 
   # eval "$@"
-  output=$( eval "$@" 2>/dev/null )     # read passed parameters of function  _w_progress  and nothing to output
-
+  output=$( eval "$@" 2>/dev/null )
   err=$?
 
   done
 
   _w_progress 100
-  printf "%s\n" "$output"               # print passed parameters of function _w_progress
+  printf "%s\n" "$output"
   # === end of function _w_progress_until
 
 }
