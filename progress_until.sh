2c495b5d7978e31baa1bd55911f2dbf0c8527127
stas-kuznetsov <89393832+stas-kuznetsov@users.noreply.github.com>
2023-05-01 13:18:22 +0300

    Update progress_until.sh

--- progress_until.sh
+++ progress_until.sh
@@ -8,9 +8,9 @@ _w_progress_until()
 {
 
   local -
-  set -o errexit
-  set -o nounset
-  set -o pipefail
+  set -o errexit     # exit immediately if command has non-zero status
+  set -o nounset     # if varuable not set - this is an error
+  set -o pipefail    #
 
   ## Draw progress bar executing code passed in arguments while it returns false.
   #
@@ -32,39 +32,40 @@ _w_progress_until()
   set +o nounset
   set +o pipefail
 
-  _w_progress 0
+   _w_progress 0
   # eval "$@"
-  output=$( eval "$@" 2>/dev/null )
+  output=$( eval "$@" 2>/dev/null )    # read passed parameters of function _w_progress and nothing to output
   err=$?
-  if [[ $err -eq 0 ]]
+  if [[ $err -eq 0 ]]     # if no errors
   then
-  _w_progress 100
-  printf "%s" "$output"
+  _w_progress 100         # run function _w_progress with his argument 100
+  printf "%s" "$output"   # print argumant _w_progress
   return
   fi
 
   now_time=$( date +%s )
   elapsed_time=$(( $now_time - $start_time ))
 
-  while (( $err != 0 && $elapsed_time < $timeout ))
+  while (( $err != 0 && $elapsed_time < $timeout ))     # while error and elapsed < timeout
   do
 
-  _w_sleep $period
+  _w_sleep $period       # run function _w_sleep with $2 argument
   # qqq2 : make argument $period proper
   # qqq2 : should return nonzero code if fail as well as print stdderr
 
   now_time=$( date +%s )
   elapsed_time=$(( $now_time - $start_time ))
-  _w_progress $(( 100 * $elapsed_time / $timeout ))
+  _w_progress $(( 100 * $elapsed_time / $timeout ))   # calculate persent of elapsed time, run function _w_progress
 
   # eval "$@"
-  output=$( eval "$@" 2>/dev/null )
+  output=$( eval "$@" 2>/dev/null )     # read passed parameters of function  _w_progress  and nothing to output
+
   err=$?
 
   done
 
   _w_progress 100
-  printf "%s\n" "$output"
+  printf "%s\n" "$output"               # print passed parameters of function _w_progress
   # === end of function _w_progress_until
 
 }
@@ -74,3 +75,4 @@ if [[ "${BASH_SOURCE[0]}" == "$0" ]]
 then
   _w_progress_until "$@"
 fi
+
