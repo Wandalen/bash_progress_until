43c95984606e0642961dc06d6c2bb7bb058d7edf
stas-kuznetsov <89393832+stas-kuznetsov@users.noreply.github.com>
2023-05-05 20:39:04 +0300

    Translate description of function

--- progress_until.sh
+++ progress_until.sh
@@ -21,20 +21,20 @@ _w_progress_until()
   ## Sample
   # _w_progress_until 30 0.25 ssh server1
   #
-  # Функція _w_progress_until() приймає команду, виконує її та вирісовує смугу прогресу роботи команди якщо команди повертає помилку (з кодом не нуль)
-  # Якщо команда відпрацьовує коректно (з кодом нуль), то функція нічого не вирісовує. Функція використовує функції _w_sleep() та _w_progress()
-  # Функція також приймае аргумент "timeout", який задає, скільки часу функція буде намагатися виконати команду. 
-  # Ще один аргумент "period" задає інтервал часу, через який функція буде повторно виконувати команду.
+  # Function `_w_progress_until` gets the command, runs it and drows the bar of work progress for command if the command returns an error (with code not equal to zero).
+  # If command works right (with code equal to zero), then function no any bar draws. The function uses another functions: `w_sleep` and `w_progress`.
+  # The function also gets the argument `timeout` that sets how many time function will trying to do the command. 
+  # Another one argument `period` sets the time interval by which function will run the command again.
   # 
-  # Аргументи:
-  # timeout - скільки часу функція буде намагатися виконати команду, в форматі float
-  # period - через який інтервал часу фйнкція буде повторно виконувати команду
-  # command - сама команда
+  # Arguments:
+  # timeout - how many time function will trying to do the command, in float format
+  # period - time interval by which function will run the command again
+  # command - command itself
   #
-  # Приклади використання функції _w_progress_until() :
-  # _w_progress_until() 30 6 ssh server1    # виконує команду ssh server1 і якщо не виходить виконати команду, то малює (а потім - перемальовує)
-  #смугу прогресу часу, який пройшов з момента початку виконання до теперешнього часу. Наприклад, через 6 секунд намалює смугу з 20% виконання, а через
-  #18 секунд буде смуга з 60% виконання. При цобму кожні 6 секунд буде повторна виконана команда ssh server1
+  # Examples usage function `_w_progress_until` :
+  # _w_progress_until() 30 6 ssh server1    # run command `ssh server1` and if command fail then draws (and then - draws again)
+  # time progress bar that has passed since start run upto now. For example, through 6 seconds will drow the bar with 20% complete and through
+  # 18 seconds will drow the bar with 60% complete. In this case every 6 seconds repeatedly will run command `ssh server1`
   #
 
   local timeout=$1
@@ -93,8 +93,8 @@ then
   _w_progress_until "$@"
 fi
 
-# Тест-кейси:
-#1. Перевіряємо, як команда в аргументі виконується ОК (з кодом 0). Для цього передаєм команду, яка повинна гарантовано відпрацювати, наприклад, ls. Потрібен для перевірки команди, яка успішно відпрацювала.
+# Test cases:
+#1. Check, how the command in argument running ОК (with code equal to zero). For this give the command that must ensuring work, for example, ls. Need for command check that successfully worked.
 #2. Перевіряємо, якщо команда в аргументі виконується з помилкою (з кодом, відмінним від нуля) і не може виконатись потім. Наприклад, ls /11, де /11 - гарантована відсутній каталог. Потрібен для перевірки команди, яка весь час дає помилку. 
 #3. Перевіряємо, якщо команда в аргументі виконується спочатку з помилкою (з кодом, відмінним від нуля) але потім виконується ОК (з кодом 0). Наприклад, ls /11, де /11 - спочатку відсутній каталог, але потім робимо його. Потрібен для перевірки команди, яка спочатку дає помилку, а потім успішно відпрацювує.
 #4. Виконуємо тест-кейс 3 з різними значеннями timeout та часом створення нового каталога: 
